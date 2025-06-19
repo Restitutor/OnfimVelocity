807358f7082466ddd52bb3f5ed4976e9759ad633
vima <bigm3z0id@gmail.com>
2025-06-19 20:39:21 +0200

    Added getting timezone by IP, provided by the TimezoneBot API

--- src/main/kotlin/me/arcator/onfimVelocity/timezone/TCPSock.kt
+++ src/main/kotlin/me/arcator/onfimVelocity/timezone/TCPSock.kt
@@ -6,21 +6,36 @@ import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.Socket
 import java.nio.charset.StandardCharsets
-import java.util.concurrent.CompletableFuture
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 
 object TCPSock {
-    private val REQUEST_TEMPLATE =
+    private val ALIAS_REQUEST_TEMPLATE =
         """{"requestType": "RequestType.TIMEZONE_FROM_ALIAS_REQUEST", "data": {"alias":"%s"}}"""
+    private val IP_REQUEST_TEMPLATE =
+        """{"requestType": "RequestType.TIMEZONE_FROM_IP_REQUEST", "data": {"ip":"%s"}}"""
+
 
     @JvmStatic
-    fun sendTZRequest(playerName: String): String? {
+    fun sendAliasTZRequest(playerName: String): String? {
+        val requestData = ALIAS_REQUEST_TEMPLATE.format(playerName.lowercase())
+        val responseMap = sendTZBotRequest(requestData) ?: return null
+        return responseMap["message"]!!.replace("\"", "")
+    }
+
+    @JvmStatic
+    fun sendIPTZRequest(ip: String): String? {
+        val requestData = IP_REQUEST_TEMPLATE.format(ip)
+        val responseMap = sendTZBotRequest(requestData) ?: return null
+        return responseMap["message"]!!.replace("\"", "")
+    }
+
+    @JvmStatic
+    private fun sendTZBotRequest(data: String): Map<String, String>? {
         return try {
             Socket("apollo", 8888).use { socket ->
-                val requestData = REQUEST_TEMPLATE.format(playerName.lowercase())
                 val out = PrintWriter(socket.getOutputStream(), true)
-                out.println(requestData)
+                out.println(data)
                 out.flush()
 
                 val response = BufferedReader(
@@ -30,16 +45,18 @@ object TCPSock {
                     )
                 ).use { it.readText() }
 
+
                 val jsonElement = Json.parseToJsonElement(response)
                 val jsonObject = jsonElement.jsonObject
 
                 val responseMap = jsonObject.mapValues { it.value.toString() }
+
                 if (responseMap["code"]!!.toInt() != 200) {
                     return null
                 }
-                responseMap["message"]?.replace("\"", "")
+                responseMap
             }
-        } catch (e: Exception) {
+        } catch (e: IOException) {
             e.printStackTrace()
             null
         }
