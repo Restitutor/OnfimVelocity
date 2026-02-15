22a6e704509639ded98b4b110c680a7d500beb36
Restitutor <nissar@mortemale.org>
2026-02-15 02:01:15 -0500

    Delete files

--- onfim-lib/src/main/kotlin/me/arcator/onfimLib/out/UnicastInterface.kt
+++
@@ -1,9 +0,0 @@
-package me.arcator.onfimLib.out
-
-import java.net.InetSocketAddress
-
-internal sealed interface UnicastInterface {
-    val type: String
-
-    fun send(message: ByteArray, host: InetSocketAddress)
-}
